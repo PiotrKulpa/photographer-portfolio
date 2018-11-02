ced8bf2c5833e7de3ee58b4d745ec6e264628db5
PiotrKulpa <p.kulpa@onet.eu>
2018-11-02 23:03:55 +0100

    test: add Portfolio tests

--- src/Portfolio.test.js
+++ src/Portfolio.test.js
@@ -7,3 +7,23 @@ import './setupTests';
 it('renders without crashing', () => {
   shallow(<Portfolio />);
 });
+
+it('have h1 element', () => {
+    const wrapper = shallow(<Portfolio />);
+    expect(wrapper.containsMatchingElement(<h1>Portfolio</h1>)).toEqual(true)
+  });
+
+it('have div with grid-sizer class', () => {
+    const wrapper = shallow(<Portfolio />);
+    expect(wrapper.containsMatchingElement(<div className="grid-sizer"></div>)).toEqual(true)
+  });
+
+it('have grid-item class', () => {
+    const wrapper = shallow(<Portfolio />);
+    expect(wrapper.find('.grid-item')).toBeTruthy()
+  });
+
+it('have many grid-item class', () => {
+    const wrapper = shallow(<Portfolio />);
+    expect(wrapper.find('.grid-item')).toBeGreaterThan(0);
+  });
