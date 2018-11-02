9a0d3b7fef3ddeef855031828b34f091cdcacbbd
PiotrKulpa <p.kulpa@onet.eu>
2018-11-02 11:02:35 +0100

    test: add App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from "enzyme";
 import App from './App';
+import Nav from './Nav';
+import Footer from './Footer';
+import './setupTests';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
+});
+
+it('includes Nav', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<Nav />)).toEqual(true)
+});
+
+it('includes Footer', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<Footer />)).toEqual(true)
 });
